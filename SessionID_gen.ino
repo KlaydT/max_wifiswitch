c12459be01b811953af2e7ea5dc0cedee7ef6756
Klayd <pmg@ukr.net>
2017-07-18 20:11:43 +0300

    session ID генератор
    Написал свой генератор session_id. Старый был громоздкий и не для этих целей. Новый - 5 строк.

--- SessionID_gen.ino
+++ SessionID_gen.ino
@@ -1,6 +1,10 @@
-void SessionID_gen() {
-  // Generate a new SSID
-  ESP8266TrueRandom.uuid(uuidNumber);
-  session_id = ESP8266TrueRandom.uuidToString(uuidNumber);
-  Serial.println("New Session_ID is: " + session_id);
+void SessionID_gen() { // Generate a new Session_ID. 
+session_id = "";
+for ( int m=0; m<30; m++ )
+  {
+  selectchar = random(0,64); //Serial.println(selectchar);
+  simbol =  pattern.substring(selectchar, selectchar+1); //Serial.print(simbol);
+  session_id +=simbol;
+  }
+Serial.println("New Session_ID is: " + session_id);
 }
